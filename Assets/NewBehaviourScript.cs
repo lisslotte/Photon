16ade45e6878c944a7e4787eb0b0077ac43e232f
lisslotte <30319456+lisslotte@users.noreply.github.com>
2018-08-31 12:43:55 +0800

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using HighlightingSystem;
+public class NewBehaviourScript : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler {
+
+   public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("enter");
+        GetComponent<Highlighter>().ConstantOn();
+    }
+  public  void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("exit");
+        GetComponent<Highlighter>().ConstantOff();
+    }
+}
